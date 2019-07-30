ee52c4d877e4f556ced3d998041f2ad8919d296e
feval <940051592@qq.com>
2019-07-30 12:02:32 +0800

    项目阶段3

---
+++ travel_web/code/travel/src/main/java/cn/bite/travel/dao/impl/RouteDaoImpl.java
@@ -0,0 +1,47 @@
+package cn.bite.travel.dao.impl;
+
+import cn.bite.travel.dao.RouteDao;
+import cn.bite.travel.domain.Route;
+import cn.bite.travel.util.JDBCUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+/**
+ * 旅游线路商品的dao的实现层
+ */
+public class RouteDaoImpl implements RouteDao {
+    //声明查询模板对象
+    private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource()) ;
+    @Override
+    public int findTotalPage(int cid) {
+
+
+        /**
+         * 通过分类id查询总记录数
+         * @param cid
+         * @return
+         */
+        //准备sql
+        String sql = "select  count(*) from tab_route where cid = ?" ;
+        //查询
+        return template.queryForObject(sql,Integer.class,cid) ;
+
+    }
+
+    /**
+     * 查询当前页面数据集合,通过分页查询
+     * @param cid
+     * @param start
+     * @param pageSize
+     * @return
+     */
+    @Override
+    public List<Route> findByPage(int cid, int start, int pageSize) {
+        //准备sql
+        String sql = "SELECT * FROM tab_route where  cid = ? limit ?,?" ;
+        //查询
+        return template.query(sql,new BeanPropertyRowMapper<Route>(Route.class),cid,start,pageSize) ;
+    }
+}
